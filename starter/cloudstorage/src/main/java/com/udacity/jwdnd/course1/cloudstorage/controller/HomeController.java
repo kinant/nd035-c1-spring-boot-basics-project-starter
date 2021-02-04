45bec50f20d1d3369b184b26003495f56e2bb1c3
KinanT <kinan.t@gmail.com>
2021-02-04 14:37:43 -0600

    Custom HTML Templates
    Attempted to move notes tab.

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -4,7 +4,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/home")
@@ -12,7 +15,20 @@ public class HomeController {
 
     @GetMapping()
     public String homeView(Model model){
-        model.addAttribute("activeTab", "files");
+        // model.addAttribute("activeTab", "files");
         return "home";
     }
+
+    @GetMapping("{tab}")
+    public String tab(@PathVariable String tab) {
+
+        System.out.println("IN TAB GET MAPPING!");
+
+        if(Arrays.asList("notes").contains(tab)){
+            return "_" + tab;
+        }
+
+        return "home";
+    }
+
 }
