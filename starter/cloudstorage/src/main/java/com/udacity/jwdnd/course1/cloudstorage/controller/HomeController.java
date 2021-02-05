13ad025b842f1b13b64a78a91ccc6eaee660a974
Kinan T <kinan.t@gmail.com>
2021-02-04 22:13:30 -0600

    Custom HTML
    Moved tabs into separate html files. Works like a charm. Also build a custom Request Interceptor so that I could log into the console the request details (URL).

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 
 @Controller
@@ -19,16 +21,28 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("{tab}")
-    public String tab(@PathVariable String tab) {
+    // https://frontbackend.com/thymeleaf/spring-boot-bootstrap-thymeleaf-tabs
+    @GetMapping("*")
+    public String tab(HttpServletRequest request) {
 
-        System.out.println("IN TAB GET MAPPING!");
+        // Extract uri from request
+        // https://stackoverflow.com/questions/10072284/requestmapping-with-multiple-values-with-pathvariable-spring-3-0/10072643
+        String uri = request.getRequestURI();
+        String tab = uri.replace("/home/", "");
 
-        if(Arrays.asList("notes").contains(tab)){
+        //TODO: Remove comments.
+        //System.out.println("===================================================");
+        //System.out.println("IN TAB GET MAPPING! TAB: " + tab);
+
+        if(Arrays.asList("files", "notes", "creds").contains(tab)){
+            //System.out.println("RETURNING TAB!");
+            //System.out.println("===================================================");
+
             return "_" + tab;
         }
 
-        return "home";
+        //System.out.println("===================================================");
+        return "empty";
     }
 
 }
