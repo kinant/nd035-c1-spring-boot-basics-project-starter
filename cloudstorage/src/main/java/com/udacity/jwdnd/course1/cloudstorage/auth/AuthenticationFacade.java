7d47b887ab114ba9d3e99675ebf0a9068a8f685e
Kinan T <kinan.t@gmail.com>
2021-02-21 22:31:12 -0600

    Commenting Pt1

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/auth/AuthenticationFacade.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/auth/AuthenticationFacade.java
@@ -6,22 +6,41 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-// https://www.baeldung.com/get-user-in-spring-security
+/**
+ * This class is used to get user authentication details.
+ * Using this method so it is accessible to entire application
+ * As seen in: https://www.baeldung.com/get-user-in-spring-security
+ */
 @Component
 public class AuthenticationFacade implements IAuthenticationFacade {
 
+    // access to user service
     @Autowired
     private UserService userService;
 
+    /**
+     * Gets the authentication.
+     * @return  returns the Authentication
+     */
     @Override
     public Authentication getAuthentication() {
+        // return authentication
         return SecurityContextHolder.getContext().getAuthentication();
     }
 
+    /**
+     * Increment a value by delta and return the new value.
+     * @return returns the authenticated user's id
+     */
     @Override
     public int getAuthenticatedUserId() {
+        // get the authentication
         Authentication authentication = getAuthentication();
+
+        // get the username
         String username = authentication.getName();
+
+        // return the userId from DB (using userService)
         return userService.getUserId(username);
     }
 }
--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/auth/IAuthenticationFacade.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/auth/IAuthenticationFacade.java
@@ -2,7 +2,11 @@ package com.udacity.jwdnd.course1.cloudstorage.auth;
 
 import org.springframework.security.core.Authentication;
 
-// https://www.baeldung.com/get-user-in-spring-security
+/**
+ * This interface is used to get user authentication details.
+ * Using this method so it is accessible to entire application
+ * As seen in: https://www.baeldung.com/get-user-in-spring-security
+ */
 public interface IAuthenticationFacade {
     Authentication getAuthentication();
     int getAuthenticatedUserId();
